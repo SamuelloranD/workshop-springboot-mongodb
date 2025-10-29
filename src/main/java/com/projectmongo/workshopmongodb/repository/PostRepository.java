5031369f1fb286d58c423aa2eb32ee4d5ec7260a
Samuel Lorand <samuel.lorand@hotmail.com>
2025-10-29 14:08:30 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/projectmongo/workshopmongodb/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.projectmongo.workshopmongodb.repository;
+
+import com.projectmongo.workshopmongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
